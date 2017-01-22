b2561877c27e117e45e3cdfeb05159a819dba9f0
aiswaryalb <aiswaryalb@gmail.com>
2017-01-22 17:40:33 +0530

    final commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,12 @@
 
 from django.conf.urls import url
-from .views import home,article_detail,admin
+from .views import home,article_detail,admin,newarticle,delete_article
 
 urlpatterns = [
     url(r'^$',home,name="home"),
-    url(r'^article/$',article_detail,name="Article detail"),
+    url(r'^article/(?P<id>[\d]+)$',article_detail,name="Article detail"),
+    url(r'^delete_article/(?P<id>[\d]+)$',delete_article,name="delete_article"),
+
     url(r'^admin/$',admin,name="admin"),
+    url(r'^article/add_new$',newarticle,name="new article"),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
 from models import Article
 
 
@@ -13,10 +13,36 @@ def home(request):
 	return render(request,'index.html',{'articles':articles})
 
 
-def article_detail(request):
-	print("Detail of the article")
-	return HttpResponse("Detail of the article")
+def article_detail(request,id):
+	try:
+		article = Article.objects.get(id = id)
+		return render(request,"article.html",{'article':article})
+	except :
+		
+		return render(request,'error.html',{})
+
+
+	
 
 def admin(request):
 	print("I am admin")
 	return HttpResponse("i am admin")
+
+def newarticle(request):
+	if request.method == 'POST':
+		newArticle = Article.objects.create(
+				title = request.POST['title'],
+				content = request.POST['content']
+			)
+		return HttpResponseRedirect('/myblog/article/'+str(newArticle.id))
+	else:
+		return render(request,'newarticle.html',{})
+
+def delete_article(request,id):
+	try:
+		article = Article.objects.get(id = id)
+		article.delete()
+		return HttpResponseRedirect('/')
+	except :
+		
+		return render(request,'error.html',{})
